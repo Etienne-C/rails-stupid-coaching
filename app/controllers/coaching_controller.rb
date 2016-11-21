f5b67cf20ed164bb47f6bf8858b6a531512ff618
Etienne Curati <etienne.curati@gmail.com>
2016-11-21 13:37:10 -0200

    Finish exercice 1

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,17 @@
+class CoachingController < ApplicationController
+
+  def answer
+    your_message = params[:query]
+    if your_message.downcase == "i am going to work right now!"
+      @answer =  ''
+    elsif your_message.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def ask
+  end
+
+end
